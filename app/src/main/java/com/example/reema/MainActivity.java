5f9465202a287b8424bd9c60567d4c01c6338733
hallashuli <hallaradwanshuli@gmail.com>
2025-09-22 11:55:52 +0300

    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

---
+++ app/src/main/java/com/example/reema/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.reema;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public  class MainActivity extends AppCompatActivity {
+    private TextView tv_Output;
+    private Button btn_Go;
+    private EditText et_Input;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.icon_plus), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+    public void onClick(View view) {
+        String input = et_Input.getText().toString();
+        tv_Output.setText(input);
+
+        Intent i=new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(i);
+
+    }
+
+    }
+
---
+++ app/src/main/java/com/example/reema/MainActivity2.java
@@ -0,0 +1,60 @@
+package com.example.reema;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+    private EditText etNum1;
+    private TextView tvOp;
+    private EditText etNum2;
+    private Button btnMul;
+    private TextView tvResult;
+    private ImageView icon_plus2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        etNum1 = findViewById(R.id.etNum1);
+        tvOp = findViewById(R.id.tvOp);
+        etNum2 = findViewById(R.id.etNum2);
+        btnMul = findViewById(R.id.btnMul);
+        tvResult = findViewById(R.id.tvResult);
+        ImageView = findViewById(R.id.icon_plus2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.icon_plus), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onClick(View v) {
+        String num1 = etNum1.getText().toString();
+        int n1 = Integer.parseInt(num1);
+        String num2 = etNum2.getText().toString();
+        int n2 = Integer.parseInt(num2);
+        int result = n1 * n2;
+        tvResult.setText(String.valueOf(result));
+
+    }
+    public void onClick2(View v){
+        String num1 = etNum1.getText().toString();
+        int n1 = Integer.parseInt(num1);
+        String num2 = etNum2.getText().toString();
+        int n2 = Integer.parseInt(num2);
+        int result = n1 + n2;
+        tvResult.setText(String.valueOf(result));
+
+    }
+}
